e7eafb9b1a786555f97d1eb403980573fadac534
Aniket52kr <aniketbawankar2021@gmail.com>
2025-04-20 19:42:43 +0530

    add frontend url

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ app.use(express.json());
 const corsOptions = {
   origin: [
     process.env.FRONTEND_URL,
+    "https://smartrecruit.onrender.com",
     "http://localhost:5173",
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
