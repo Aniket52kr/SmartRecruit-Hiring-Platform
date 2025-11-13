9efc13379ff84ebecc54324c0fd3a45cce67bf83
Aniket52kr <aniketbawankar2021@gmail.com>
2025-11-14 01:35:27 +0530

    add backend url

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://13.235.214.243:3000";
 
 if (!BACKEND_URL) {
   throw new Error("VITE_BACKEND_URL is not defined in environment variables!");
