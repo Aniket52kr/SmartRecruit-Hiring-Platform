bd03a120a7387ffbc509f7bc3c80341599c238a2
Aniket52kr <aniketbawankar2021@gmail.com>
2025-11-14 00:43:29 +0530

    check backend url exist

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
+if (!BACKEND_URL) {
+  throw new Error("VITE_BACKEND_URL is not defined in environment variables!");
+}
+
+
 // Create an Axios instance
 const api = axios.create({
   baseURL: `${BACKEND_URL}/api`, // automatically adds /api to all requests
