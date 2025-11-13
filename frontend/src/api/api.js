2e31f080a7914c6a415f15a61a995ad9efcf606a
Aniket52kr <aniketbawankar2021@gmail.com>
2025-11-14 02:20:25 +0530

    update

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,15 +1,7 @@
 import axios from "axios";
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://13.235.214.243:3000";
-
-if (!BACKEND_URL) {
-  throw new Error("VITE_BACKEND_URL is not defined in environment variables!");
-}
-
-
-// Create an Axios instance
 const api = axios.create({
-  baseURL: `${BACKEND_URL}/api`, // automatically adds /api to all requests
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
